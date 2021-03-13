a18479596d99a01fdb07113c7b28fc5fb05a7b6d
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2021-03-13 17:04:21 -0300

    specifyng /users route and saving data by json on database

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,11 +1,33 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
 
 class UserController{
 
-	async create(request: Request, response: Response){
-		const body = request.body;
-		console.log(body);
-		return response.send();
+	async create(request: Request, response: Response)
+	{
+		const { name, email } = request.body;
+		const userRepository = getRepository(User); // entidade criada em 'models/' essa é a 'conexão' entre o meu código e a tabela do banco.. nesse exemplo é a tabela User
+
+		// SELECT * FROM users WHERE email = $email;
+		const userAlreadyExists = await userRepository.findOne({
+			email
+		});
+
+		if (userAlreadyExists)
+		{
+			return response.status(400).json({error: "User alreaty exists!"});
+		}
+		else
+		{
+			/* crio uma alteração... */
+			const user = userRepository.create({
+				name,
+				email
+			});
+			/* dou um 'commit' na alteração realizada acima com o método .save() */
+			return response.send(await userRepository.save(user));
+		}
 	}
 }
 
--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -23,4 +23,4 @@ class User {
 	}
 }
 
-export { User }
+export { User };
