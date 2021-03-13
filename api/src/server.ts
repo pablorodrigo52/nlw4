402cbd39674376378cd861311531c8dd2a29544f
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2021-03-13 15:22:29 -0300

    creating sqlite database

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,12 @@
+import 'reflect-metadata';
 import express from  'express';
+import './database';
+
 
 const app = express();
+
+app.get("/", (request, response)=>{
+	response.send("Hello World");
+});
 
 app.listen(3333, () => console.log("Server is running on port 3333!"));
